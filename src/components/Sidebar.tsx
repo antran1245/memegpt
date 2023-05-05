c8a35564e44dff62b5f8f56edd337f074697be14
An Tran <antran1245@gmail.com>
2023-05-05 14:20:45 -0700

    done making the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,21 +5,23 @@ import { faMessage, faUserCircle } from "@fortawesome/free-regular-svg-icons"
 export default function Sidebar() {
     return(
         <div className="bg-black h-[100vh] w-[345px] flex justify-center text-white">
-            <div className="mt-[14px]">
-                <button className="rounded-[20px] border-2 text-[22px] h-[78px] w-[323px] flex-auto">
+            <div className="mt-[14px] flex flex-col">
+                <button className="rounded-[20px] border-2 text-[22px] h-[78px] w-[323px] flex-none">
                     <FontAwesomeIcon icon={faPlus} className="mr-[43px]"/>
                     Find New Meme
                 </button>
-                <div className="mt-[17px]">
-                    <p className="mb-[20px]">Today</p>
-                    <div className="flex justify-between items-center">
-                        <div className="flex items-center">
-                            <FontAwesomeIcon icon={faMessage} className="mr-[16px]"/>
-                            <p>Annie Sucks</p>
-                        </div>
-                        <div>
-                            <FontAwesomeIcon icon={faPencil} className="mr-[7px]"/>
-                            <FontAwesomeIcon icon={faTrashCan}/>
+                <div className="mt-[17px] flex flex-col flex-auto justify-between">
+                    <div className="flex-none">
+                        <p className="mb-[20px]">Today</p>
+                        <div className="flex justify-between items-center">
+                            <div className="flex items-center">
+                                <FontAwesomeIcon icon={faMessage} className="mr-[16px]"/>
+                                <p>Annie Sucks</p>
+                            </div>
+                            <div>
+                                <FontAwesomeIcon icon={faPencil} className="mr-[7px]"/>
+                                <FontAwesomeIcon icon={faTrashCan}/>
+                            </div>
                         </div>
                     </div>
                     <div className="flex justify-between items-center">
