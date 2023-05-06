57421d6b4b6cea9d59ff0b22882ec84eba78a52f
An Tran <antran1245@gmail.com>
2023-05-05 19:24:25 -0700

    added more styles to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,14 +4,14 @@ import { faMessage, faUserCircle } from "@fortawesome/free-regular-svg-icons"
 
 export default function Sidebar() {
     return(
-        <div className="bg-black h-[100vh] w-[345px] flex justify-center text-white">
-            <div className="mt-[14px] flex flex-col">
-                <button className="rounded-[20px] border-2 text-[22px] h-[78px] w-[323px] flex-none">
+        <div className="bg-black h-[100vh] w-[345px] flex text-white">
+            <div className="mt-[14px] flex flex-col w-[100%]">
+                <button className="rounded-[20px] border-2 text-[22px] h-[78px] w-[323px] flex-none mx-auto">
                     <FontAwesomeIcon icon={faPlus} className="mr-[43px]"/>
                     Find New Meme
                 </button>
                 <div className="mt-[17px] flex flex-col flex-auto justify-between">
-                    <div className="flex-none">
+                    <div className="flex-none pr-[16px] pl-[22px]">
                         <p className="mb-[20px]">Today</p>
                         <div className="flex justify-between items-center">
                             <div className="flex items-center">
@@ -24,12 +24,12 @@ export default function Sidebar() {
                             </div>
                         </div>
                     </div>
-                    <div className="flex justify-between items-center">
+                    <div className="flex justify-between items-center py-[19px] pl-[18px] pr-[22px] border-t-[1.5px]">
                         <div className="flex items-center">
-                            <FontAwesomeIcon icon={faUserCircle} className="mr-[21px]"/>
-                            <p>Annie Belle</p>
+                            <FontAwesomeIcon icon={faUserCircle} size="2xl" className="mr-[21px]"/>
+                            <p className="text-[20px] font-medium">Annie Belle</p>
                         </div>
-                        <FontAwesomeIcon icon={faEllipsis}/>
+                        <FontAwesomeIcon icon={faEllipsis} size="2xl"/>
                     </div>
                 </div>
             </div>
