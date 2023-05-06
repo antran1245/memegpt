21eaebee83a1197df6f4c54ced9e6c9f0c89207c
An Tran <antran1245@gmail.com>
2023-05-05 18:51:01 -0700

    design the intro page section

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,30 +1,31 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
+import styles from '@/styles/intro.module.css'
 
 export default function Intro() {
     return(
-        <div className="flex flex-col items-center flex-auto">
-            <div className="flex flex-col items-center">
-                <h1>MemeGPT</h1>
-                <div className="flex justify-center">
+        <div className="flex flex-col items-center flex-auto justify-between">
+            <div className="flex flex-col items-center mt-[183px]">
+                <h1 className="text-[48px] font-semibold">MemeGPT</h1>
+                <div className="flex justify-evenly">
                     <div className="w-[30%] text-center">
-                        <span>&#x1F389;</span>
-                        <p>Examples</p>
-                        <p className="">Input as many emojis that you want and it will generate a meme for you</p>
+                        <span className={styles.icon}>&#x1F389;</span>
+                        <p className={styles.caption}>Examples</p>
+                        <p className={styles.textBox}>Input as many emojis that you want and it will generate a meme for you</p>
                     </div>
                     <div className="w-[30%] text-center">
-                        <span>&#128170;</span>
-                        <p>Capabilities</p>
-                        <p className="">Generate meme from emojis</p>
+                        <span className={styles.icon}>&#128170;</span>
+                        <p className={styles.caption}>Capabilities</p>
+                        <p className={styles.textBox}>Generate meme from emojis</p>
                     </div>
                     <div className="w-[30%] text-center">
-                        <span>&#x1F913;</span>
-                        <p>Limitations</p>
-                        <p className="">Might create a not accurate meme.</p>
+                        <span className={styles.icon}>&#x1F913;</span>
+                        <p className={styles.caption}>Limitations</p>
+                        <p className={styles.textBox}>Might create a not accurate meme.</p>
                     </div>
                 </div>
             </div>
-            <div>
+            <div className={styles.inputBox}>
                 <input type="text" placeholder="Input Emoji"/>
                 <FontAwesomeIcon icon={faPaperPlane}/>
             </div>
