9f9037983b8989dc67fcd203ee7cbf0d0a62cd44
An Tran <antran1245@gmail.com>
2023-05-26 14:41:15 -0700

    working on display a list of emoji for input

--- src/components/InputEmoji.tsx
+++ src/components/InputEmoji.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faPaperPlane } from "@fortawesome/free-regular-svg-icons"
+import { faPaperPlane, faFaceSmile } from "@fortawesome/free-regular-svg-icons"
 import styles from '@/styles/intro.module.css'
 
 interface InputEmojiProps {
@@ -10,7 +10,14 @@ export default function InputEmoji( { onInput } : InputEmojiProps ) {
     return(
         <div className={styles.inputBox}>
             <input type="text" placeholder="Input Emoji"/>
-            <FontAwesomeIcon icon={faPaperPlane} onClick={onInput}/>
+            <div className={styles.iconBox}>
+                <div className={styles.popupIconBox}>
+                    <div className={styles.popupIcon}>hi</div>
+                    <span className={styles.popupTriangle}></span>
+                </div>
+                <FontAwesomeIcon icon={faFaceSmile} />
+                <FontAwesomeIcon icon={faPaperPlane} onClick={onInput}/>
+            </div>
         </div>
     )
 }
