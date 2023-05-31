58d24e7afb8895ba00ff683089b3fcf406f0c984
An Tran <antran1245@gmail.com>
2023-05-30 21:51:45 -0700

    emojis are being shown in the container

--- src/components/InputEmoji.tsx
+++ src/components/InputEmoji.tsx
@@ -1,18 +1,26 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPaperPlane, faFaceSmile } from "@fortawesome/free-regular-svg-icons"
 import styles from '@/styles/intro.module.css'
+import { useEffect, useState } from "react"
 
 interface InputEmojiProps {
     onInput: () => void
 }
 
 export default function InputEmoji( { onInput } : InputEmojiProps ) {
+    const [emoji, setEmoji] = useState<any>(null)
+    useEffect(() => {
+        fetch('/api/emojiDataset')
+        .then(resp => resp.json())
+        .then(data => setEmoji(data.dataset))
+        .catch(err => console.log(err))
+    }, [])
     return(
         <div className={styles.inputBox}>
             <input type="text" placeholder="Input Emoji"/>
             <div className={styles.iconBox}>
                 <div className={styles.popupIconBox}>
-                    <div className={styles.popupIcon}>hi</div>
+                    <div className={styles.popupIcon}>{emoji[0]}</div>
                     <div className={styles.popupTriangle}></div>
                 </div>
                 <FontAwesomeIcon icon={faFaceSmile} />
--- src/pages/api/emojiDataset.ts
+++ src/pages/api/emojiDataset.ts
@@ -2,13 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import emoji from "emojilib"
 
 type Data = {
-    name: string
+    dataset: any
 }
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    console.log(Object.keys(emoji), Object.keys(emoji).length)
-    res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({ dataset: Object.keys(emoji) })
 }
